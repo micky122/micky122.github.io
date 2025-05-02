6b2f6ba2f5989012832eba42fec015c458d3c474
micky122 <juliannakamoto122@gmail.com>
2025-05-02 06:03:15 +0200

    portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Julian Nakamoto </span>
+            from <span className="purple">Germany.</span>
+            <br />
+            I am currently employed as a software developer at Arvato Systems.
+            <br />
+            I have completed Integrated MSc (IMSc) in Maths and Computing.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
